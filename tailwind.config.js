064fbb56c41acd079185f647ca0753b06b380c0d
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-06 09:27:14 +0530

    Added some theme colors primaty andd secondary and added darkmode property

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode:"class",
   theme: {
     extend: {
       colors:{
-        primary:"#ffc001"
+        primary:"#ffc001",
+        secondary:"#ff9c01"
       },
       container:{
         center:true,
