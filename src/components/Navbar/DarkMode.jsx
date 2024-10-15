5da9467bfd9218624755c3e2264b1e8a309ba109
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-15 10:09:50 +0530

    Darmode button funtionality added.

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -31,8 +31,15 @@ function DarkMode(){
     return(
         <>
             <div className="relative">
-                <img src={darkIcon} alt="" className="w-12 absolute right-0 z-10 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300" onClick={changeTheme}/>
-                <img src={lightIcon} alt="" className="w-12 drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300" onClick={changeTheme}/>
+                <img src={darkIcon} 
+                alt="" 
+                className={`w-12 absolute right-0 z-10 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 ${theme === "dark" ? "opacity-0" : "opacity-100"}`}
+                onClick={changeTheme}/>
+
+                <img src={lightIcon} 
+                alt="" 
+                className="w-12 drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300" 
+                onClick={changeTheme}/>
             </div>
         </>
     )
