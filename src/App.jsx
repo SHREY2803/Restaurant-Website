4928be52fe260c8607c5eb5f010e6f93c034f4ab
Shrey Kulkarni <shrey280302@gmail.com>
2024-10-15 22:58:33 +0530

    Banner component added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero.jsx";
 import Services from "./components/Services/Services.jsx";
+import Banner from "./components/Banner/Banner.jsx";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Navbar />
       <Hero />
       <Services />
+      <Banner />
     </div>
   )
 }
