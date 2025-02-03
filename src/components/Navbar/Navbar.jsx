37bdb3f7efad5cbc852654add091a240adcb5383
Shrey Kulkarni <shrey280302@gmail.com>
2025-02-03 19:14:22 +0530

    Added testimonial and contact us object for linking.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,41 +16,46 @@ const Menu = [
   },
   {
     id: 3,
-    name: "About",
-    link: "/#about",
+    name: "Testimonial",
+    link: "/#testimonial",
+  },
+  {
+    id: 4,
+    name: "Contact Us",
+    link: "/#contact",
   },
 ];
 const Navbar = () => {
   return (
     <>
-      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <div className="duration-200 bg-white shadow-md dark:bg-gray-900 dark:text-white">
         <div className="container py-3 sm:py-0">
-          <div className="flex justify-between items-center">
+          <div className="flex items-center justify-between">
             <div>
-              <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <a href="#" className="flex gap-2 text-2xl font-bold sm:text-3xl">
                 <img src={Logo} alt="Logo" className="w-10" />
                 Eatery Zone
               </a>
             </div>
-            <div className="flex justify-between items-center gap-4">
+            <div className="flex items-center justify-between gap-4">
               <div>
                 <DarkMode />
               </div>
-              <ul className="hidden sm:flex items-center gap-4">
+              <ul className="items-center hidden gap-4 sm:flex">
                 {Menu.map((menu) => (
                   <li key={menu.id}>
                     <a
                       href={menu.link}
-                      className="inline-block py-4 px-4 hover:text-yellow-500"
+                      className="inline-block px-4 py-4 hover:text-yellow-500"
                     >
                       {menu.name}
                     </a>
                   </li>
                 ))}
               </ul>
-              <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3">
+              <button className="flex items-center gap-3 px-4 py-1 text-white duration-200 rounded-full bg-gradient-to-r from-primary to-secondary hover:scale-105">
                 Order
-                <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+                <FaCartShopping className="text-xl text-white cursor-pointer drop-shadow-sm" />
               </button>
             </div>
           </div>
